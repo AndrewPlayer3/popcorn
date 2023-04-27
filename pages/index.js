b8ea25f164ce31303a162004582971698d42a4ae
Andrew Player <andrewplayer3@gmail.com>
2023-04-27 12:46:41 -0500

    Dont display videos when not logged in

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
+import loginStatus from '../helpers/login-status'
+
 import Results from '../components/Results.js'
 import Layout from '../components/Layout.js'
+import LoginForm from '../components/LoginForm.js'
 
 export async function getServerSideProps(context) {
     var qry = ''
@@ -22,25 +27,37 @@ export async function getServerSideProps(context) {
 }
 
 export default function Home({ videos }) {
-    if (videos.length === 0) {
-        return (
-            <h1 className="absolute left-1/4 h-2/4 w-2/4 pt-10 text-center text-2xl text-slate-200">
-                Sorry, we could not find any videos matching that search.
-            </h1>
-        )
-    }
+    const { data: session, status } = useSession()
+    const router = useRouter()
+    
     return (
         <>
-            {' '}
-            {/* Result is the json file of video data/ */}
-            <Results
-                result={videos}
-                classes={
-                    'h-auto px-5 mt-4 text-white sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6'
-                }
-            />
-        </>
-    )
+        {loginStatus(status, router, false) ? (
+            <>
+                {(videos.length === 0) ? (
+                    <h1 className="absolute left-1/4 h-2/4 w-2/4 pt-10 text-center text-2xl text-slate-200">
+                        Sorry, we could not find any videos matching that search.
+                    </h1>
+                ): (
+                <>
+                    {' '}
+                    {/* Result is the json file of video data/ */}
+                    <Results
+                        result={videos}
+                        classes={
+                            'h-auto px-5 mt-4 text-white sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 3xl:grid-cols-6'
+                        }
+                    />
+                </>)}
+            </>
+        ) : (
+            <>
+                <div className="flex justify-center">
+                    <LoginForm />
+                </div>
+            </>
+        )}    
+    </>)
 }
 
 Home.layout = Layout
